3453790e8a29a5fa9ca7a90737081cf60a7009be
Arslon <arslonniyozmetov@gmail.com>
2025-04-19 19:53:04 +0500

    Yangilandi

--- home/views.py
+++ home/views.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator
+from .models import News  # Agar News modeli shu joyda bo‘lsa
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,8 +9,14 @@ def about_view(request):
     return render(request, 'about.html')
 def contact_view(request):
     return render(request, 'contact.html')
+
 def news_view(request):
-    return render(request, 'news.html')
+    news_list = News.objects.all().order_by('-date')
+    paginator = Paginator(news_list, 9)  # Har sahifada 9 ta xabar
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'news.html', {'page_obj': page_obj})
+
 def gallery_view(request):
     return render(request, 'gallery.html')
 def teacher_view(request):
