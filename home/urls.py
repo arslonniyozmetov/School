ed34036eeea4d5496196fdb5f261c8764ffc2fd3
Arslon <arslonniyozmetov@gmail.com>
2025-04-27 18:15:41 +0500

    Yangilandi

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,16 @@
+from django.conf import settings
 from django.urls import path
-from .views import *
-from .views import news_list, news_detail
+from.views import *
+from django.conf.urls.static import static
 
-
-urlpatterns = (
+urlpatterns = [
     path('', home_view, name='home'),
     path('about/', about_view, name='about'),
     path('contact/', contact_view, name='contact'),
-    path('teacher/',teacher_view, name='teacher'),
-path('news/', news_list, name='news'),
+    path('teacher/', teacher_view, name='teacher'),
+    path('news/', news_list, name='news'),
     path('news/<slug:slug>/', news_detail, name='news_detail'),
-)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
